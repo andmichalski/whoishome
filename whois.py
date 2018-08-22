88853bc163fc38263c9394bf8e5ced8d15b76b0f
andmichalski <andrzmichal@gmail.com>
2018-08-22 21:39:33 +0200

    Refractoring code

--- whois.py
+++ whois.py
@@ -8,7 +8,7 @@ class WhoIs():
     def _get_home_macs(self):
         home_macs = {}
         with open('home_macs.txt', 'r') as f:
-            for line in f:
+            for line in f.read().splitlines():
                 record = line.split(" ")
                 MAC = record[1]
                 IP = record[0]
@@ -19,20 +19,19 @@ class WhoIs():
     def _open_active_macs(self):
         active_macs = {}
         with open('active_macs.txt', 'r') as f:
-            for line in f:
+            for line in f.read().splitlines():
                 record = line.split(" ")
-                if len(record) == 2:
+                if ":" in record[1]:
                     MAC = record[1]
                     IP = record[0]
                     active_macs[MAC] = IP
                 else:
                     self.my_ip = record[0]
-                    del active_macs[record[0]]
         return active_macs
 
     def compare_macs(self):
         new_macs = []
-        for active_mac in self.active_macs:
+        for active_mac in self.active_macs.keys():
             if active_mac in self.home_macs:
                 self.home_macs[active_mac][2] = True
             else:
@@ -40,7 +39,8 @@ class WhoIs():
         return new_macs
 
     def get_inmates_status(self):
-        inmates = [(value[1], value[2]) for (_, value) in self.home_macs.items()]
+        inmates = [(value[1], value[2]) for (_, value) in
+                   self.home_macs.items()]
         return inmates
 
     def print_inmates_status(self):
@@ -54,6 +54,6 @@ class WhoIs():
 
 if __name__ == "__main__":
     w = WhoIs()
-    print(w.compare_macs())
+    w.compare_macs()
     w.print_inmates_status()
-    #TODO remove files
+    # TODO remove files
--- whois.sh
+++ whois.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sudo nmap -n -sP 192.168.2.1/24 | awk '/Nmap scan report for /{printf $5x;printf " ";getline;getline;print $3x}' > active_macs.txt
-my_ip=$(hostname -I)
-echo "$my_ip" >> active_macs.txt
+sudo nmap -n -sP 192.168.1.1/24 | awk '/Nmap scan report for /{printf $5x;printf " ";getline;getline;print $3x}' > active_macs.txt
+echo "$(hostname -I)$(ip a  | grep -A 1 wlo1: | awk '/link/{printf $2x}')" >> active_macs.txt
 python whois.py
