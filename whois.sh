c6b1d44148af8c66352b91817f8d1fb06d877265
andmichalski <andrzmichal@gmail.com>
2018-08-23 21:17:07 +0200

    Corrections on Raspberry Server

--- whois.sh
+++ whois.sh
@@ -3,7 +3,7 @@
 while : 
 do
 	sudo nmap -n -sP 192.168.1.1/24 | awk '/Nmap scan report for /{printf $5x;printf " ";getline;getline;print $3x}' > active_macs.txt
-	echo "$(hostname -I)$(ip a  | grep -A 1 wlo1: | awk '/link/{printf $2x}')" >> active_macs.txt
+	echo "$(hostname -I)$(ip a  | grep -A 1 wlan0: | awk '/link/{printf $2x}' | tr [a-z] [A-Z])" >> active_macs.txt
 	clear
 	python whois.py 
 done
